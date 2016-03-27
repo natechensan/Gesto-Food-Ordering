a523489080be1ad28215f68b7914cbbc8c9add04
Ziran Chen <ziranchen@Zirans-MacBook-Pro.local>
2016-03-27 16:08:09 -0400

    updated bonus.txt

--- app/models/food.js
+++ app/models/food.js
@@ -1,6 +1,4 @@
 var mongoose = require('mongoose');
-require('mongoose-double')(mongoose);
-var SchemaTypes = mongoose.Schema.Types;
 module.exports = mongoose.model('Food', {
     name: {
         type: String,
